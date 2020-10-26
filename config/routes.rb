2fa73fb71f1ac1ece3ff6eac7b3a5c22134f161d
Joaquin Mellado Quevedo <mellado.joaquin@gmail.com>
2020-10-26 17:31:35 -0300

    added a rest arq to routes

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,9 @@ Rails.application.routes.draw do
   post 'byebye', to: 'greetings#bye', as: 'chao'
 
   get 'say-hello', to: 'greetings#say_hello'
+
+  post :add, to: 'calculator#compute_add'
+  get :add, to: 'calculator#add'
   
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
