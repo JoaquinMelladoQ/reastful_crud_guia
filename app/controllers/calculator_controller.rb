afbb7cbe5da713551abd14ac8e5d0002f64947a3
Joaquin Mellado Quevedo <mellado.joaquin@gmail.com>
2020-10-26 17:37:16 -0300

    added methods to new file

---
+++ app/controllers/calculator_controller.rb
@@ -0,0 +1,12 @@
+class CalculatorController < ApplicationController
+
+    def add
+        
+    end
+
+    def compute_add
+        first = params[:first]to_i
+        last = params[:last]to_i
+        @total = first + last
+    end
+end
