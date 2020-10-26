91b5c4f49897659f366ad7ce378c21cd67536efa
Joaquin Mellado Quevedo <mellado.joaquin@gmail.com>
2020-10-26 11:50:15 -0300

    added routes to

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,10 @@
 Rails.application.routes.draw do
+  # RUTAS POST
   post 'greetings', to: 'greetings#hello'
+  post 'byebye', to: 'greetings#bye', as: 'chao'
+
+  # RUTAS GET
+  get 'say-hello', to: 'greetings#say_hello'
+  
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
